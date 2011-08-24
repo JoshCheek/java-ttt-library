7ecda7629c9d2100127b6597b197c21077666723
Josh Cheek <josh.cheek@gmail.com>
2011-08-24 07:51:40 -0500

    Initial setup generated by IntelliJ

---
+++ src/TTT.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshuajcheek
+ * Date: 8/24/11
+ * Time: 7:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TTT {
+    public static void main() {
+
+    }
+}
---
+++ test/TTTTest.java
@@ -0,0 +1,12 @@
+/**
+* Created by IntelliJ IDEA.
+* User: joshuajcheek
+* Date: 8/24/11
+* Time: 7:50 AM
+* To change this template use File | Settings | File Templates.
+*/
+public class TTTTest extends junit.framework.TestCase {
+    public void setUp() throws Exception {
+
+    }
+}
