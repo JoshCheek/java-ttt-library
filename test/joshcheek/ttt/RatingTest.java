28c2d3777a4c4b75f4996cff5bab7eac948cd0c6
Josh Cheek <josh.cheek@gmail.com>
2011-08-25 14:46:10 -0500

    Create Rating class

---
+++ test/joshcheek/ttt/RatingTest.java
@@ -0,0 +1,11 @@
+package joshcheek.ttt;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshuajcheek
+ * Date: 8/25/11
+ * Time: 2:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RatingTest {
+}
